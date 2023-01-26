7ae553aee00c93874ca9d01019e295e2e623136f
Allen Zhao <allenz2423@hstat.org>
2023-01-25 20:28:59 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    use text_io::scan;
+    let board = [[false, false, false]
+            ,[false,false,false]
+            ,[false,false,false]];
+    for i in 0..board.len() {
+        println!("Array {:?}", board[i]);
+    }
+}
